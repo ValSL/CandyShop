b6b57870a2634214c07abcdcbde243ddd2cd2d81
valid <valid.saley@gmail.com>
2020-02-20 21:40:00 +0300

    time limit decorator

--- CandyShop_engine/orders/views.py
+++ CandyShop_engine/orders/views.py
@@ -2,8 +2,21 @@ from django.shortcuts import render
 from cart.cart import Cart
 from .forms import OrderCreateForm
 from django.core.mail import send_mail
+from datetime import datetime
 
 
+def time_limit_decorator(func):
+    def wrapper(request):
+        now_hour = datetime.now().hour + 3
+        if now_hour >= 22 or now_hour <= 8:
+            return render(request, 'orders/time_limit.html')
+        else:
+            return func(request)
+
+    return wrapper
+
+
+@time_limit_decorator
 def order_create(request):
     cart = Cart(request)
     user = request.user
